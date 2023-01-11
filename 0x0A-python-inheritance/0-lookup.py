e089393f2fb55d61182047f025a708d51af4cff1
Hoyeander <oyindamolayemisi@gmail.com>
2023-01-11 13:08:09 -0800

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-def lookup(obj):
-    """ Function that returns the list of available attributes
-        and methods of an object
-    Args:
-        obj: instance of the class
-    Returns:
-        List of attributes
-    """
+"""
+module for lookup.
+"""
 
+
+def lookup(obj):
+    """
+    the list of available attributes and
+    methods of an object
+    """
     return dir(obj)
